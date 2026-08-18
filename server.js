ebca69d3acea54fcd75e39388d570e366ebcd9f8
Great246 <greatrichard687@gmail.com>
2026-08-18 07:43:06 +0100

    Fix url

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 // app.use(helmet()) 
 
 app.use(cookieParser());
-app.set('trust proxy', 1)
+
 
 
 app.use("/api/auth", authRouter);
@@ -39,6 +39,7 @@ app.use("/api/gallery", galleryRouter)
 app.use("/api/contact", contactRouter)
 app.use("/api/visit", visitorrouter)
 
+app.set('trust proxy', 1)
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
